6c7881c541b9408d953099eea984b7ac0a046bcb
Adrian1426 <adrianhh1426@gmail.com>
2021-01-21 01:10:28 -0600

    configuracion usuarios

---
+++ src/main/java/com/servicios/app/usuarios/SpringbootServiciosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.servicios.app.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiciosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiciosUsuariosApplication.class, args);
+	}
+
+}
