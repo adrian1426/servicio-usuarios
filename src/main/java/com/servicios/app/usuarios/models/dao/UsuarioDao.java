ceeac695e8698183d99f6901671482f8eb2ef772
Adrian1426 <adrianhh1426@gmail.com>
2021-01-31 16:20:07 -0600

    exportando método y renombrando path,params con search

--- src/main/java/com/servicios/app/usuarios/models/dao/UsuarioDao.java
+++ src/main/java/com/servicios/app/usuarios/models/dao/UsuarioDao.java
@@ -2,14 +2,17 @@ package com.servicios.app.usuarios.models.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.servicios.app.usuarios.models.entity.Usuario;
 
 @RepositoryRestResource(path="usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
 	
-	public Usuario findByUsername(String username);
+	@RestResource(path="findUsername") //nombre del path o recurso
+	public Usuario findByUsername(@Param("user") String username); //param para renombrar el query
 	
 	@Query("select u from Usuario u where u.username=?1")
 	public Usuario getUserByUsername(String username);
