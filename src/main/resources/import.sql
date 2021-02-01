e75c34f611dd5ca1dd3c523a1c464b8835be7765
Adrian1426 <adrianhh1426@gmail.com>
2021-02-01 15:37:32 -0600

    update password a bcrypt tipo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `usuarios` (username,password,enabled,nombre,apellido,email) VALUES('Adrian1995','Ahh123',1,'Adrian','Hernandez','a@gmail.com');
-INSERT INTO `usuarios` (username,password,enabled,nombre,apellido,email) VALUES('JJ123','123',1,'José','Hernandez','jose@gmail.com');
+INSERT INTO `usuarios` (username,password,enabled,nombre,apellido,email) VALUES('Adrian1995','$2a$10$xsKG3FUJcaP7clGzmTHhYeXcRe53Ic0Z5nix87zlVSPhOriBrEqdq',1,'Adrian','Hernandez','a@gmail.com');
+INSERT INTO `usuarios` (username,password,enabled,nombre,apellido,email) VALUES('JJ123','$2a$10$U98kUn6GpO/5H5nN0ZeE3uDfo1sHUoJWO6UMvvpujNVsGIqySy8Ye',1,'José','Hernandez','jose@gmail.com');
 
 INSERT INTO `roles` (nombre) VALUES('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES('ROLE_ADMIN');
